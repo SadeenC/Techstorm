4b6aa1bb18fb1226f296c3e72e91b9c1d324f551
SadeenC <bsadeen9@gmail.com>
2022-09-07 15:24:06 +0530

    first one

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+const App = () =>{
+  return(
+    <div className="App">
+      <h2>Home page</h2>
+      <post/>
+    </div>
+  )
+}
+
+
+export default App;
---
+++ src/post.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import './post.css';
+
+class post extends Component{
+    render(){
+        return(
+        <div className="post">
+            <h2>Hello</h2>
+        </div>
+        );
+    }
+}
+export default post;
